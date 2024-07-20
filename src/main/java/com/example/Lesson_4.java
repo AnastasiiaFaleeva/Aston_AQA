2d0fde99aea3773fa3e91bc0f948db195bf1e70f
AnastasiiaFaleeva <113059067+AnastasiiaFaleeva@users.noreply.github.com>
2024-07-20 14:56:42 +0300

    задача 1

---
+++ src/main/java/com/example/Lesson_4.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class Lesson_4 {
+
+    public static void main(String[] args) {
+        printThreeWords();
+
+    }
+//Задача 1
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+}
